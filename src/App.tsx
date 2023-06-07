4f3dba964139692da52756c89e0e5654b37c99e5
Teo Turner <teoturner@gmail.com>
2023-06-07 19:13:30 +0100

    getting the sketch canvas setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { SketchCanvas } from "./components/SketchCanvas";
+
+function App() {
+  return (
+    <div className="App">
+      <SketchCanvas />
+    </div>
+  );
+}
+
+export default App;
